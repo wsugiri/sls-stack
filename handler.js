cadd52429948fc7341da7943e8641adc1237b4ac
wsugiri <wsugiri@gmail.com>
2019-08-09 08:10:43 +0700

    .

--- handler.js
+++ handler.js
@@ -18,4 +18,35 @@ exports.func2 = async (data, event) => {
   return { data, event };
 };
 
+exports.app = async (event) => {
+  const { action } = event.headers;
+
+  if (action && exports[action]) {
+    const data = {
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body ? JSON.parse(event.body) : ''
+    };
+
+    const resp = await exports[action](data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(resp)
+    }
+  }
+
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ message: '-- lambda not found --', event }),
+  };
+};
+
+exports.tambah = async (data) => {
+  return { message: 'tambah', data };
+};
+
+exports.kali = async (data) => {
+  return { message: 'kali', data };
+};
+
 module.exports = exports;
