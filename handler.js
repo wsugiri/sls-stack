c094e67129f122ba419c116230ac628f0a36931c
wsugiri <wsugiri@gmail.com>
2019-08-09 07:25:52 +0700

    .

--- handler.js
+++ handler.js
@@ -10,15 +10,12 @@ exports.hello = async (event, context) => {
 exports.func1 = async (data) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: '-- call func1 --', data }),
+    body: JSON.stringify({ message: '--- call func1 ---', data }),
   };
 };
 
-exports.func2 = async (data) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: '-- call func2 --', data }),
-  };
+exports.func2 = async (data, event) => {
+  return { data, event };
 };
 
 module.exports = exports;
