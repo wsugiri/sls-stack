aeb3bfb46bd806c16ed51ad8797e9e2f27399c41
wsugiri <wsugiri@gmail.com>
2019-08-14 20:14:34 +0700

    .

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-AWS.config.region = 'us-east-1';
+// AWS.config.region = 'us-east-1';
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
